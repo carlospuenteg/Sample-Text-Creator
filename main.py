b65580c6c730b37c078eb83bd3f287d9b5be7ac0
Carlos Puente <carlospuenteg@gmail.com>
2022-05-20 16:48:07 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,55 @@
 from googletrans import Translator
+import os
 import json
 
-# Create the translator
-translator = Translator()
+def create_texts():
+    # Create folder if it doesn't exist
+    if not os.path.exists("texts"):
+        os.mkdir("texts")
+        
 
-# Import languages
-languages = json.load(open('languages.json'))
+    # Create the translator
+    translator = Translator()
 
-# Import sample text
-sample = open("sample.txt", "r").read()
-sampleArr = sample.splitlines()
-divSample = []
+    # Import languages
+    languages = json.load(open('languages.json'))
 
-
-
-toAppend = ""
-# For each line in the sample
-for line in sampleArr:
-
-    # If the appended text would be < 5000 characters
-    if len(toAppend) + len(line) + 1 < 5000:
-        toAppend += (line + "\n")
-    else:
-        divSample.append(toAppend)
-        toAppend = (line + "\n")
-
-# Add the last text to append (without the \n)
-divSample.append(toAppend)
+    # Import sample text
+    sample = open("sample.txt", "r").read()
+    sampleArr = sample.splitlines()
+    divSample = []
 
 
 
-# Write the translation to the files
-for lang in languages.keys():
-    toWrite = ""
+    toAppend = ""
+    # For each line in the sample
+    for line in sampleArr:
 
-    for lines in divSample:
-        if (lines != ""):
-            print(lang)
-            toWrite += (translator.translate(lines, dest=lang).text + "\n")
+        # If the appended text would be < 5000 characters
+        if len(toAppend) + len(line) + 1 < 5000:
+            toAppend += (line + "\n")
+        else:
+            divSample.append(toAppend)
+            toAppend = (line + "\n")
 
-    f = open(lang + ".txt", "a")
-    f.write(toWrite[:-1])
-    f.close()
+    # Add the last text to append (without the \n)
+    divSample.append(toAppend)
+
+    # Iterate through (keys and values) in languages object 
+
+
+    # Write the translation to the files
+    for lang, name in languages.items():
+        toWrite = ""
+
+        for lines in divSample:
+            if (lines != ""):
+                print(lang)
+                toWrite += (translator.translate(lines, dest=lang).text + "\n")
+
+        f = open("texts/" + name + ".txt", "a")
+        f.write(toWrite[:-1])
+        f.close()
+
+
+create_texts()
