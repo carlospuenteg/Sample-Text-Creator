be9dc936005161a4d38e20a5f74bf030c6a4edbd
Carlos Puente <carlospuenteg@gmail.com>
2022-05-20 16:44:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ languages = json.load(open('languages.json'))
 sample = open("sample.txt", "r").read()
 sampleArr = sample.splitlines()
 
+divSample = []
+
+print(sampleArr)
+
+for i in range(0,len(sampleArr),20):
+    divSample.append("\n".join(sampleArr[i:i+10]))
+
 
 for lang in languages.keys():
-    for line in sampleArr:
-        print(lang)
-        f = open(lang + ".txt", "a")
-        f.write(translator.translate(sample, dest=lang).text)
-        f.close()
+    for lines in divSample:
+        if (lines != ""):
+            print(lang)
+            f = open(lang + ".txt", "a")
+            f.write(translator.translate(lines, dest=lang).text + "\n")
+            f.close()
